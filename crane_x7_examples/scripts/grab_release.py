08c8d118f1a3e6d32359fa8d10f40f9d44c3c4ac
Masato Kubotera <s19c1040hd@s.chibakoudai.jp>
2020-12-14 22:47:20 +0900

    add node Exclusion

--- crane_x7_examples/scripts/grab_release.py
+++ crane_x7_examples/scripts/grab_release.py
@@ -13,6 +13,7 @@ from std_msgs.msg import Int32  # メッセージ型
 from move_def import arm_move   # 指定座標に手先を動かす関数
 from move_def import hand_move  # ハンドの角度[rad]を指定し動かす関数
 
+flag0 = True # 動作フラグ0
 flag1 = True # 動作フラグ1
 flag2 = True # 動作フラグ2
 
@@ -23,7 +24,7 @@ arm.set_max_velocity_scaling_factor(0.1) # 実行速度
 
 
 def grab_release(data):
-    global flag1, flag2, arm
+    global flag0, flag1, flag2, arm
 
     seal_x = 0.30           # x座標[m]
     seal_y = -0.15          # y座標[m]
@@ -35,6 +36,53 @@ def grab_release(data):
     hand_open = math.pi/4   # ハンド 開く角度[rad]
 
     pub = rospy.Publisher("report", Int32, queue_size = 1) # 動作報告パブリッシャ
+
+    if data.data == "Exclusion" and flag0 :
+        rospy.loginfo("Start Exclusion")
+        flag0 = False
+        # --------------------
+        while len([s for s in rosnode.get_node_names() if 'rviz' in s]) == 0:
+            rospy.sleep(1.0)
+        rospy.sleep(1.0)
+        # --------------------
+        arm_initial_pose = arm.get_current_pose().pose # アーム初期ポーズを表示
+        # --------------------
+        # 跳ね除ける
+        push_x1 = 0
+        push_x2 = 0.10
+        push_x3 = 0.20
+        push_y1 = -0.25
+        push_y2 = -0.20
+        push_before_z = 0.20
+        push_after_z = 0.12
+        hand_open = math.pi/4   # ハンド 開く角度[rad]
+
+        arm.set_named_target("home")
+        arm.go()
+        arm_move(push_x1, push_y1, push_before_z)
+        hand_move(hand_open)
+        arm_move(push_x1, push_y1, push_after_z)
+        rospy.sleep(0.5)
+        arm_move(push_x1, push_y1, push_before_z)
+        for n in range(4):
+            if n % 2 == 0:
+                arm_move(push_x2, push_y1, push_before_z)
+                arm_move(push_x2, push_y1, push_after_z)
+                rospy.sleep(0.5)
+                arm_move(push_x2, push_y1, push_before_z)
+            else:
+                arm_move(push_x3, push_y1, push_before_z)
+                rospy.sleep(0.5)
+        hand_move(0.1)
+        arm_move(push_x3, push_y2, push_before_z)
+        for i in range(10):
+            arm_move(push_x3, push_y2, push_after_z)
+            push_y2 = push_y2 - 0.01
+        # --------------------
+        # 動作終了報告
+        pub.publish(True)
+        rospy.loginfo("Finish Exclusion")
+        # --------------------
 
     if data.data == "Grab" and flag1 :
         rospy.loginfo("Start Grab")
