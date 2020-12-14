21e5485c5b1656e334d5efc1d5d596604a8d3f04
Kazuki Shirasu <s19C1057FA@s.chibakoudai.jp>
2020-12-14 11:00:10 +0900

    update a file

--- crane_x7_examples/scripts/robotdesign3_team5.py
+++ crane_x7_examples/scripts/robotdesign3_team5.py
@@ -31,6 +31,15 @@ def main():
     seal_after_z = 0.30     # 掴む後  Z座標[m]
     seal_close = 0.10       # 掴む角度[rad]
     # --------------------
+    # 跳ね除ける
+    push_x1 = 0
+    push_x2 = 0.10
+    push_x3 = 0.20
+    push_y1 = -0.25
+    push_y2 = -0.20
+    push_before_z = 0.20
+    push_after_z = 0.12
+    # --------------------
     # 初期設定
     hand_open = math.pi/4   # ハンド 開く角度[rad]
     ofset_exec_speed = 0.1  # 実行速度 
@@ -80,6 +89,7 @@ def main():
     arm_initial_pose = arm.get_current_pose().pose
     print("Arm initial pose:")
     print(arm_initial_pose)
+    '''
     # --------------------
     # 挨拶
     pub.publish(1)
@@ -90,6 +100,31 @@ def main():
     pub.publish(2)
     while Moveflag != 2 :
         pass
+    '''
+    # --------------------
+    # 跳ね除ける
+    arm.set_named_target("home")
+    arm.go()
+    arm_move(push_x1, push_y1, push_before_z)
+    hand_move(hand_open)
+    arm_move(push_x1, push_y1, push_after_z)
+    rospy.sleep(0.5)
+    arm_move(push_x1, push_y1, push_before_z)
+    for n in range(4):
+        if n % 2 == 0:
+            arm_move(push_x2, push_y1, push_before_z)
+            arm_move(push_x2, push_y1, push_after_z)
+            rospy.sleep(0.5)
+            arm_move(push_x2, push_y1, push_before_z)
+        else:
+            arm_move(push_x3, push_y1, push_before_z)
+            rospy.sleep(0.5)
+    hand_move(0.1)
+    arm_move(push_x3, push_y2, push_before_z)
+    for i in range(10):
+        arm_move(push_x3, push_y2, push_after_z)
+        push_y2 = push_y2 - 0.01
+    '''
     # --------------------
     # はんこを掴む
     pub.publish(3)
@@ -117,7 +152,7 @@ def main():
         pass
     # --------------------
     pub.publish(0)
-
+    '''
 
     # SRDFに定義されている"home"の姿勢にする
     arm.set_named_target("home")
